192704dbca7cd984ce154857935a3b5558e14f83
Kamil Urbańczyk <vixerin@outlook.com>
2023-11-08 09:21:37 +0100

    Further code refactoring

--- TeamBuilder/PrismStartup.cs
+++ TeamBuilder/PrismStartup.cs
@@ -12,19 +12,16 @@ namespace TeamBuilder
         public static void Configure(PrismAppBuilder builder)
         {
             builder.RegisterTypes(RegisterTypes)
-                .OnAppStart("MainPage");
+                .OnAppStart("TeamMembersPage");
         }
 
         private static void RegisterTypes(IContainerRegistry containerRegistry)
         {
-            containerRegistry.RegisterForNavigation<MainPage, MainPageViewModel>();
             containerRegistry.RegisterForNavigation<TeamMembersPage, TeamMembersViewModel>();
             containerRegistry.RegisterForNavigation<AddTeamMembersPage, AddTeamMembersViewModel>();
 
             containerRegistry.Register<ITeamMembersRepository, TeamMembersRepository>();
             containerRegistry.Register<IAlertService, AlertService>();
-
-            //containerRegistry.RegisterSingleton<IPopupNavigation, PopupNavigation>();
         }
     }
 }
